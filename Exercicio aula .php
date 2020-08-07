586fc1232422a45d88600627689120b89dec8ba4
Tatslap <69019340+Tatslap@users.noreply.github.com>
2020-08-07 11:31:11 -0300

    Add files via upload

---
+++ Exercicio aula .php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Exercicio de PHP</title>
+</head>
+
+<body>
+	
+	<?php
+			
+		if ($nummber % 2 == 0){
+			echo "O número é par";
+		}else{
+			echo "O número é ímpar";
+		}
+		
+	?>
+	
+	
+	
+</body>
+</html>
